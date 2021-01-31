8418db924f1ea18d505e96d96ad3959068ea9dfa
Jeremy Dunne <jeremymdunne@gmail.com>
2021-01-31 11:49:47 -0500

    working on simulation

--- SimulationKinematics.py
+++ SimulationKinematics.py
@@ -1,8 +1,25 @@
 
 rocket_data = {
-    "dry_mass": 1, 
+    "major_od" : 1,
+    "length" : 1, 
+    "dry_mass" : 1, 
+    "drag_reference_area" : 1,
+    "drag_cd_v" : [], 
+    "motor" : {
+        "motor_file_name" : "",
+    }
     ""
 }
+
+simulation_settings = {
+    "time_stemp" : 0.01, 
+    "start_asl" : 0, 
+    "start_attitude" : 0
+    
+}
+
+
+
 
 
 class SimulationKinematics:
