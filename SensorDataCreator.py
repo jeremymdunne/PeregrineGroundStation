6b681cad40001364f12d27ce8498447ee23a3a70
Jeremy <jeremymdunne@gmail.com>
2021-02-01 09:41:49 -0500

    working kinematic simulation

---
+++ SensorDataCreator.py
@@ -0,0 +1,22 @@
+class SensorDataCreator: 
+    def __init__(self, flight_data):
+        # break down the flight data into individual arrays 
+
+        # create the sensor data at a given time step for each sensor 
+        
+
+
+    def create_pressure_sensor_data(self, altitude_data, time_step):
+        # simulate pressure sensor data 
+        # make the data lag behind the actual altitude 
+        # add gaussian noise 
+        simulated_pressure 
+
+
+    def create_accel_sensor_data(self, acceleration_data, time_step): 
+        # simulate acceleration data 
+        # give the data a small scale offset 
+        # add gaussian noise 
+        simulated_acceleration 
+
+    
